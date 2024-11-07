d652c963da2a0dfa096c25efc7ffd1a08aeb04ef
EduardoCilvetiUA <eacilveti@miuandes.cl>
2024-11-07 13:04:03 -0300

    reorganize build scripts and simplify configuration

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,17 +2,16 @@
 # exit on error
 set -o errexit
 
-# Install system dependencies required for pg gem
-curl -sSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
-echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
-apt-get update
-apt-get install -y libpq-dev postgresql-client
+# Install PostgreSQL development files
+apt-get update -qq
+apt-get install -y postgresql-client postgresql-server-dev-all build-essential libpq-dev
 
 # Clear tmp/cache
 rm -rf tmp/cache/*
 
-# Build gem native extensions with explicit pg config
-gem install pg -v '1.5.9' -- --with-pg-config=/usr/bin/pg_config
+# Build dependencies
+bundle config build.pg --with-pg-config=/usr/bin/pg_config
+bundle config set --local path 'vendor/bundle'
 
 bundle install
 bundle exec rake assets:clobber # Clear any old assets
