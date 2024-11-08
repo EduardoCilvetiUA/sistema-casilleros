f5308457623837107fc4b85b3f54012aa784fbb4
EduardoCilvetiUA <eacilveti@miuandes.cl>
2024-11-08 01:14:26 -0300

    Configure Action Mailer for production with SMTP settings and default URL options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,4 +99,24 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # Mail configuration for production
+  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:              'gmail.com',
+    user_name:           'lalocilveti@gmail.com',
+    password:            ENV['EMAIL_PASSWORD'],
+    authentication:      'plain',
+    enable_starttls_auto: true
+  }
+
+  config.action_mailer.default_url_options = { 
+    host: 'plankton-app-viibg.ondigitalocean.app',
+    protocol: 'https'
+  }
 end
